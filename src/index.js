40c07335503ac212e3b5b97a39cf4f7b9f776b8c
Meron Ogbai <okbaymeron@gmail.com>
2020-12-15 01:44:04 +0300

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import contact from './contact';
 import menu from './menu';
 
 const content = document.querySelector('#content');
-// home(content);
-// contact(content);
-menu(content);
+
+const homeTab = document.querySelector('.home-tab');
+const contactTab = document.querySelector('.contact-tab');
+const menuTab = document.querySelector('.menu-tab');
+const tabs = [homeTab, contactTab, menuTab];
+
+home(content);
+
+homeTab.addEventListener('click', () => {
+  content.innerHTML = '';
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+  homeTab.classList.toggle('active');
+  home(content);
+});
+
+contactTab.addEventListener('click', () => {
+  content.innerHTML = '';
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+  contactTab.classList.toggle('active');
+  contact(content);
+});
+
+menuTab.addEventListener('click', () => {
+  content.innerHTML = '';
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+  menuTab.classList.toggle('active');
+  menu(content);
+});
